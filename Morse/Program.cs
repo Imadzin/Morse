046529103be8c260825e37e3f39c30777fc7c9f7
Lex-ion <killkiisdog@gmail.com>
2022-11-05 11:43:07 +0100

    Přidejte soubory projektu.

---
+++ Morse/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Morse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            while (true)
+            {
+                Menu();
+            }
+
+            
+            
+            
+            
+            
+            
+        }
+
+
+       public static void Menu()
+       {
+            Console.Clear();
+            Console.WriteLine("Menu:");
+            Console.WriteLine();
+            Console.WriteLine("Překlad      [1]");
+            Console.WriteLine("Procvičování [2]");
+            Console.WriteLine("Nastavení    [3]");
+            Výběr();
+       }
+
+
+        public static void Výběr()
+        {
+
+            string text =Convert.ToString( Console.ReadKey().KeyChar);
+            int ciselnyVyber;
+
+            if (int.TryParse(text, out ciselnyVyber)) 
+            {
+            ciselnyVyber=Convert.ToInt32(text);
+            }else
+                Menu();
+
+
+
+                switch (ciselnyVyber)
+            {
+                default:
+                    Menu();
+                    break;
+
+                case 1:
+                    Trans.Start();
+                    break;
+
+                case 2:
+
+                    break;
+
+                case 3:
+                    
+                    break;
+
+
+
+
+
+            }
+
+
+
+
+        }
+
+
+
+    }
+}
---
+++ Morse/Trans.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morse
+{
+    internal class Trans
+    {
+        public static void Start()
+        {
+            Console.Clear();
+            Console.WriteLine("Zadej text či kód pro překlad.");
+            string vstup = Console.ReadLine();
+            Sort(vstup);
+        }
+
+
+
+        public static void Sort(String txt)
+        {
+            char prvniPismeno = txt[0];
+            char druhePismeno = txt[1];
+
+
+            if (prvniPismeno == '.' || prvniPismeno == '-')
+            {
+                ZMorse(txt);
+            }
+            else
+                DoMorse(txt);
+
+
+        }
+
+
+        public static void ZMorse(string txt)
+        {
+
+        }
+
+        public static void DoMorse(string txt)
+        {
+
+        }
+
+
+
+    }
+}
