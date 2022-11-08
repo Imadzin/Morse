8711c1eab828a51d317bcf28ef7b9f90d44bc599
Lex-ion <killkiisdog@gmail.com>
2022-11-08 18:21:19 +0100

    Změna pípání - false/true

--- Morse/ConfigByUser.cs
+++ Morse/ConfigByUser.cs
@@ -15,11 +15,73 @@ namespace Morse
             Console.Clear();
             Console.WriteLine("Nastavení:");
             Console.WriteLine();
-            Console.WriteLine("Pípání - " + Config.pipani + "[1]");
+            Console.WriteLine("Pípání - " + Config.pipani + " [1]");
             Console.WriteLine("Délka pípnutí tečky: "+Config.delkaPipnutiTecka+"ms [2]");
             Console.WriteLine("Délka pípnutí čárky: "+Config.delkaPipnutiCarka+"ms [3]");
             Console.WriteLine("Délka odmlčení po znaku: "+Config.delkaLomeno+"ms [4]");
-            Console.WriteLine("Délka odm");
+            Console.WriteLine("Délka odmlčení po mezeře: " + Config.delkaMezera+"ms [5]");
+            Console.WriteLine("Frekvence pípání: " + Config.frekvencePipani + "hz [6]");
+           string volba = Console.ReadLine();
+            int volbaInt;
+
+            if (int.TryParse(volba,out volbaInt))
+            {
+                volbaInt=Convert.ToInt32(volba);
+            }else
+            {
+                Start();
+            }
+
+            switch (volbaInt)
+            {
+                default:
+                    Start();
+                    break;
+
+                case 1:
+                    ConfigByUserWrite(0,Convert.ToString( Config.pipani = !(Config.pipani)));
+                    Start();
+                    break;
+
+                case 2:
+                    BT("Zadej hodnotu:", ConsoleColor.DarkRed);
+                    break;
+                   
+            }
+
+
+        }
+
+
+        public static void ConfigByUserWrite(int line, string txt)
+        {
+            string[] countOfLines = File.ReadAllLines("Config.txt");
+            
+            using (StreamWriter SW = new StreamWriter("Config.txt"))
+            {
+
+               for (int currentLine = 0; currentLine <= countOfLines.Length-1; currentLine++)
+                {
+                   
+
+                   if (currentLine == line)
+                   {
+                        SW.WriteLine(txt);
+                   }else
+                   {
+                        SW.WriteLine(countOfLines[currentLine]);
+                   }
+                   
+                    
+                }
+
+                SW.Flush();
+
+            }
+        }
+
+        public static void BT(string txt, ConsoleColor barva)
+        {
 
         }
 
